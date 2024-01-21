26ffa77b91206d598f951ddf2e5d9c0a8f912f78
Matic Ozimic <matic.ozimic@gmail.com>
2024-01-21 23:52:31 +0100

    ain.yml file za actions

--- obj/Debug/net6.0/Fitness.AssemblyInfo.cs
+++ obj/Debug/net6.0/Fitness.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80dded1706c236cfd6ec5dfd296e60c5d9ac2e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fitness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
